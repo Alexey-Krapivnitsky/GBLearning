0474533e78d64f927cdb416fb0ee186b83950d55
Alexey-Krapivnitsky <cronos1009@yandex.ru>
2020-02-05 14:14:04 +0500

    Mission 2 complete

--- Вебинар_Основы/lesson1/Alexey_Krapivnitskiy/less1_1.py
+++
@@ -1,5 +0,0 @@
-user_time = int(input('Введите время в секундах: '))
-user_hour = user_time // 3600
-user_minute = (user_time % 3600) // 60
-user_second = (user_time % 3600) % 60
-print(f'Вы ввели {user_hour} часов {user_minute} минут {user_second} секунд')
---
+++ Вебинар_Основы/lesson1/Alexey_Krapivnitskiy/less1_3.py
@@ -0,0 +1,5 @@
+# user_number = int(input('Введите число от 0 до 9: '))
+user_number = 7
+out_number = 'Сумма {0} + {0}{0} + {0}{0}{0} составляет {1}'.format(user_number, user_number * 123)
+print(out_number)
+
