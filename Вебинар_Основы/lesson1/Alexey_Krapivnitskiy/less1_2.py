6b0b6066c27e032d05d7ece4a37272be0e184951
Alexey-Krapivnitsky <cronos1009@yandex.ru>
2020-02-05 15:42:25 +0500

    Небольшие исправления

--- Вебинар_Основы/lesson1/Alexey_Krapivnitskiy/less1_2.py
+++ Вебинар_Основы/lesson1/Alexey_Krapivnitskiy/less1_2.py
@@ -1,8 +1,11 @@
 # user_time = int(input('Введите время в секундах: '))
 user_time = 21753
+
+# вычисляем количество часов, минут и секунд
 user_hour = user_time // 3600
 user_minute = (user_time % 3600) // 60
 user_second = (user_time % 3600) % 60
+
 # формирование склонения часов, минут и секунд
 if user_hour in [1]:
     hour_symbol = 'час'
@@ -22,5 +25,6 @@ elif user_second in [2, 3, 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54]:
     sec_symbol = 'секунды'
 else:
     sec_symbol = 'секунд'
+
 # Вывод результата
 print(f'Вы ввели {user_hour} {hour_symbol} {user_minute} {min_symbol} {user_second} {sec_symbol}')
--- Вебинар_Основы/lesson1/Alexey_Krapivnitskiy/less1_4.py
+++ Вебинар_Основы/lesson1/Alexey_Krapivnitskiy/less1_4.py
@@ -1,10 +1,13 @@
 # user_number = input('Введите целое положительное число: ')
 user_number = '4387'
-num_digit = len(user_number) - 1
-max_digit = int(user_number[num_digit])
+
+num_digit = len(user_number) - 1  # номер цифры в числе, начальный - последняя цифра
+max_digit = int(user_number[num_digit])  # предполагаем, что максимальная - последняя цифра
+
 while num_digit > 0:
     difference = max_digit - int(user_number[num_digit - 1])
     if difference < 0:
         max_digit = int(user_number[num_digit - 1])
     num_digit -= 1
+
 print(f'Максимальная цифра в числе {user_number} - цифра "{max_digit}"')
